030a6299d24cf259dd1dd0514480fbfa75a35ee0
swathiperla <98946392+perlaritgithub@users.noreply.github.com>
2022-03-17 17:29:30 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,20 @@
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+let odds = arr.filter(n => n%2 == 1)
+
+console.log(odds)
+
+
+
+
+/*function palindrome(str) {
+    let splitted = str.split("");
+    let reversed = splitted.reverse("");
+    let joined = reversed.join("");
+    
+return joined.toLowerCase().replace(/[^0-9a-z]/gi, '') == str.toLowerCase().replace(/[^0-9a-z]/gi, '');
+
+}*/
+
+
+
